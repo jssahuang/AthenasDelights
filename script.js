091f1f3324d20dc1152926e4a94eb9097038c033
Jessica Huang <huangjt@usc.edu>
2024-03-02 15:24:17 -0800

    added files

---
+++ script.js
@@ -0,0 +1,8 @@
+function clicked() {
+    console.log("clicked")
+    let userInput = document.getElementById("user-input").value;
+    fetch("http://127.0.0.1:5000/input/" + userInput).then(response => response.json()).then(response => {
+        console.log(response);
+        document.getElementById('display').innerHTML = response.user
+    })
+}
