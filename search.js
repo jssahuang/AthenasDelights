169e700cb977d274a12348a602d6d8d4dde287c1
Jessica Huang <huangjt@usc.edu>
2024-03-03 05:38:49 -0800

    edit

--- search.js
+++ search.js
@@ -39,6 +39,57 @@ function search() {
     .catch(error => console.error('Error fetching data:', error));
 }
 
+function display() {
+    fetch("http://127.0.0.1:5000/getRecipes/").then(response => response.json()).then(response => {
+        if (Object.keys(response).length !== 0) {
+            document.getElementById("sweet-tod-id").style.display = "block";
+            // document.getElementById("sweet-tod-id").innerHTML = "";
+            console.log("got here");
+        }
+        let myDisplay = document.getElementById("sweet-tod-id");
+        for (let i = 0; i < Object.keys(response).length; i++) {
+            let content = document.createElement("div");
+            content.className = "sweet-tod-content";
+            let myNameDiv = document.createElement("div");
+            myNameDiv.className = "name";
+            let myIngredientsDiv = document.createElement("div");
+            myIngredientsDiv.className = "ingredients";
+            let myRecipesDiv = document.createElement("div");
+            myRecipesDiv.className = "recipe";
+
+            let name = document.createElement("p");
+
+            name.innerHTML = response[i].name;
+            let ingredients = document.createElement("p");
+            ingredients.innerHTML = response[i].ingredients;
+            let recipe = document.createElement("p");
+            recipe.innerHTML = response[i].instructions;
+            myNameDiv.appendChild(name);
+            myIngredientsDiv.appendChild(ingredients);
+            myRecipesDiv.appendChild(recipe);
+            content.appendChild(myNameDiv);
+            content.appendChild(myIngredientsDiv);
+            content.appendChild(myRecipesDiv);
+            myDisplay.appendChild(content);
+        //      div sweet-tod, div sweet-tod-content, div name, div ingredients, div recipe,
+        }
+        // if (Object.keys(response).length >= 1) {
+        //     let name = document.getElementById("name");
+        //     name.innerHTML = "<p>" + response[0].name + "</p>";
+        //     let ingredients = document.getElementById("ingredients");
+        //     ingredients.innerHTML = "<p>Ingredients</p><p>" + response[0].ingredients + "</p>"
+        //     let recipe = document.getElementById("recipe");
+        //     recipe.innerHTML = "<p>Recipe</p><p>" + response[0].instructions + "</p>"
+        // }
+
+    })
+
+}
+
+
+
+display();
+
 // function clicked() {
 //     console.log("clicked")
 //     let userInput = document.getElementById("user-input").value;
