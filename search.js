ad7a1aad03023fa4b5ab8c8f40353580321a6351
anchals7 <anchalsr@usc.edu>
2024-03-02 22:56:02 -0800

    search html and css updates

--- search.js
+++ search.js
@@ -1,6 +1,8 @@
 function search() {
     console.log("search clicked");
     let userInput = document.getElementById("search").value;
+    var text = document.getElementById("sweet-tod");
+    text.style.display = "block";
     fetch("http://127.0.0.1:5000/search/" + userInput).then(response => response.json()).then(response => {
         console.log(response);
         let myDisplay = document.getElementById("display");
