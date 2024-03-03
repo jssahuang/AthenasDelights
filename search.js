d94bb85473756e77b8362f42b9b03a6d660f2915
Kashvi Jayagiri <154042588+kashvirjayagiri@users.noreply.github.com>
2024-03-02 23:52:16 -0800

    updated backend 1-1

--- search.js
+++ search.js
@@ -7,12 +7,34 @@ function search() {
         console.log(response);
         let myDisplay = document.getElementById("display");
         let val = (response);
-        let myRecipe = val.hits[0].recipe.label;
-        console.log(myRecipe);
-        myDisplay.innerHTML = myRecipe;
 
+        let myRecipe_label = val.hits[0].recipe.label;
+       console.log(myRecipe_label);
+       let myRecipe_ingredients = val.hits[0].recipe.ingredientLines;
+       console.log(myRecipe_ingredients);
+        let myRecipe_image = val.hits[0].recipe.image;
+        console.log(myRecipe_image);
+        let myRecipe_cuisine = val.hits[0].recipe.cuisineType;
+        console.log(myRecipe_cuisine);
+        let myRecipe_dietLabels = val.hits[0].recipe.dietLabels;
+        console.log(myRecipe_dietLabels);
+        let myRecipe_url = val.hits[0].recipe.url;
+        console.log(myRecipe_url);
+
+        let iframe = document.createElement('iframe');
+        iframe.src = myRecipe_url;
+        iframe.width = '100%';
+        iframe.height = '600';
+        iframe.frameBorder = '0';
+
+
+        myDisplay.innerHTML = '';
+        myDisplay.innerHTML = "<p>" + myRecipe_label + "<br>" + myRecipe_ingredients + "<br>"
+        + "<img src='"+ myRecipe_image + "' alt= 'it is a dessert.'>" + "<br>"
+        + myRecipe_cuisine + "<br>" + myRecipe_dietLabels + "<br>" + "</p>";
+        myDisplay.appendChild(iframe);
     })
-
+    .catch(error => console.error('Error fetching data:', error));
 }
 
 // function clicked() {
