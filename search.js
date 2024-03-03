f10eed6583a9c70c265be994ad6788912d34f529
Jessica Huang <huangjt@usc.edu>
2024-03-03 08:46:39 -0800

    edit

--- search.js
+++ search.js
@@ -1,19 +1,15 @@
 function search() {
     console.log("search clicked");
     let userInput = document.getElementById("search").value;
-    var text = document.getElementById("sweet-tod");
-    text.style.display = "block";
-    var textx = document.getElementById("image-container");
-    textx.style.display = "block";
+    var myDisplay = document.getElementById("sweet-tod-id");
+    myDisplay.style.display = "block";
     fetch("http://127.0.0.1:5000/search/" + userInput).then(response => response.json()).then(response => {
         console.log(response);
-        let myDisplay = document.getElementById("display");
         let val = (response);
-
         let myRecipe_label = val.hits[0].recipe.label;
-       console.log(myRecipe_label);
-       let myRecipe_ingredients = val.hits[0].recipe.ingredientLines;
-       console.log(myRecipe_ingredients);
+        console.log(myRecipe_label);
+        let myRecipe_ingredients = val.hits[0].recipe.ingredientLines;
+        console.log(myRecipe_ingredients);
         let myRecipe_image = val.hits[0].recipe.image;
         console.log(myRecipe_image);
         let myRecipe_cuisine = val.hits[0].recipe.cuisineType;
@@ -22,19 +18,11 @@ function search() {
         console.log(myRecipe_dietLabels);
         let myRecipe_url = val.hits[0].recipe.url;
         console.log(myRecipe_url);
-
-        let iframe = document.createElement('iframe');
-        iframe.src = myRecipe_url;
-        iframe.width = '100%';
-        iframe.height = '600';
-        iframe.frameBorder = '0';
-
-
         myDisplay.innerHTML = '';
-        myDisplay.innerHTML = "<p>" + myRecipe_label + "<br>" + myRecipe_ingredients + "<br>"
-        + "<img src='"+ myRecipe_image + "' alt= 'it is a dessert.'>" + "<br>"
-        + myRecipe_cuisine + "<br>" + myRecipe_dietLabels + "<br>" + "</p>";
-        myDisplay.appendChild(iframe);
+        myDisplay.innerHTML =  `<div class="sweet-tod-content"><img src="${myRecipe_image}" id="resultsImg" /><div id="name"><p>${myRecipe_label}</p></div><div class="ingredients"><p>${myRecipe_ingredients}</p></div><div class="recipe"><p>${val.hits[0].recipe.url}</p></div></div> </div>`
+        // myDisplay.innerHTML = "<p>" + myRecipe_label + "<br>" + myRecipe_ingredients + "<br>"
+        // + "<img src='"+ myRecipe_image + "' alt= 'it is a dessert.'>" + "<br>"
+        // + myRecipe_cuisine + "<br>" + myRecipe_dietLabels + "<br>" + "</p>";
     })
     .catch(error => console.error('Error fetching data:', error));
 }
